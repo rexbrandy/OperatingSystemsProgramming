76fb12e24c7f10c7e96171d78c880c6b81507962
rexbrandy <bailey.sdev@gmail.com>
2024-09-04 09:12:36 +1000

    Almost finished CP

--- case_study4/cp1.c
+++ case_study4/cp1.c
@@ -16,8 +16,9 @@
 #define BUFFERSIZE 4096
 #define COPYMODE 0644
 
+int check_file(char *src_file, char *dst_file);
+int get_file_permission(char *src_file);
 void oops(char *, char *);
-int check_file(char src_file, char dst_file);
 
 int main(int ac, char *av[])
 {
@@ -26,68 +27,66 @@ int main(int ac, char *av[])
 	int n_chars;
 	char buf[BUFFERSIZE];
 
-	if (ac != 3)
-	{
+    int src_st_mode;
+
+    struct stat src, dst;
+    int src_err, dst_err;
+
+    if (ac != 3){
 		fprintf(stderr, "usage: %s source destination\n", *av);
 		exit(1);
 	}
+    
+    src_err = stat(av[1], &src);
+    dst_err = stat(av[2], &dst);
 
-	if ((in_fd = open(av[1], O_RDONLY)) == -1)
-	{
+    if (src_err == -1 | dst_err == -1) {
+        oops("Error getting stat", (src_err == -1) ? av[1] : av[2] );
+    }
+
+	if ((in_fd = open(av[1], O_RDONLY)) == -1) {
 		oops("Cannot open ", av[1]);
 	}
 
-	if ((out_fd = creat(av[2], COPYMODE)) == -1)
-	{
-		oops("Cannot creat", av[2]);
-	}
-
-    if (check_file(av[1], av[2])  == -1) {
-        oops("Files are the same", "");
+    if (src.st_ino == dst.st_ino) {
+        // If inode number of files is the same
+        // then they are the same file
+        // and we don't need to do anything
+        exit(1);
     }
 
-	while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0)
-	{
-		if (write(out_fd, buf, n_chars) != n_chars)
-		{
+	if (S_ISDIR(src.st_mode)) {
+		if ((out_fd = creat(av[2], src.st_mode)) == -1){
+			oops("Cannot creat into directory", av[2]);
+		}
+	} else {
+		// Here the permissions are used from the source file
+		// `src.st_mode` = src file permissions
+		if ((out_fd = creat(av[2], src.st_mode)) == -1){
+			oops("Cannot creat", av[2]);
+		}
+	}
+
+
+	while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0){
+		if (write(out_fd, buf, n_chars) != n_chars){
 			oops("Write error to ", av[2]);
 		}
 	}
 
-	if (n_chars == -1)
-	{
+	if (n_chars == -1){
 		oops("Read error from ", av[1]);
 	}
 
-	if (close(in_fd) == -1 || close(out_fd) == -1)
-	{
+	if (close(in_fd) == -1 || close(out_fd) == -1){
 		oops("Error closing files", "");
 	}
 
 	return 0;
 }
 
-void oops(char *s1, char *s2)
-{
+void oops(char *s1, char *s2){
 	fprintf(stderr, "Error: %s ", s1);
 	perror(s2);
 	exit(1);
-}
-
-int check_file(char src_file, char dst_file)
-{
-    // If inode number of files is the same
-    // then they are the same file
-    struct stat src, dst;
-    int src_err, dst_err;
-    
-    src_err = stat(src_file, &src);
-    dst_err = stat(dst_file, &dst);
-
-    if (src.st_ino == dst.st_ino) {
-        // if they're the same return -1
-        return -1;
-    }
-    // else return 1
-    return 1;
 }
